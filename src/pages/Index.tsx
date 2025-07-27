256031bf5e2d3b1b66c37edae80674a294616c40
tawbazf <tawbazehaf@gmail.com>
2025-07-27 14:16:36 +0100

    feat: implement Index page with all core sections using alias imports

---
+++ src/pages/Index.tsx
@@ -0,0 +1,21 @@
+import HeroSection from "../components/HeroSection";
+import TechStackSection from "../components/TechStackSection";
+import ProjectsSection from "../components/ProjectsSection";
+import AboutSection from "../components/AboutSection";
+import TestimonialsSection from "../components/TestimonialsSection";
+import ContactSection from "../components/ContactSection";
+
+const Index = () => {
+  return (
+    <div className="min-h-screen bg-background overflow-x-hidden">
+      <HeroSection />
+      <TechStackSection />
+      <ProjectsSection />
+      <AboutSection />
+      <TestimonialsSection />
+      <ContactSection />
+    </div>
+  );
+};
+
+export default Index;
